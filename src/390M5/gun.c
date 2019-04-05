919845717254d5000b72d5dc3731476dd01d3c30
Joseph <jmiera@byu.edu>
2019-04-05 14:41:35 -0600

    implemented shooting only when we have ammo

--- src/390M5/gun.c
+++ src/390M5/gun.c
@@ -3,11 +3,15 @@
 #include "transmitter.h"
 
 #define MAX_SHOTCOUNT 10
+#define NO_SHOTS_LEFT 0
+#define RESET 0
+#define SHOT_SUCCESSFUL true
+#define SHOT_UNSUCCESSFUL false
 
 enum gun_st_t {
 	init_st,
 	wait_st,
-	shoot_st,
+	shot_st,
 	force_reload_st,
 	auto_reload_st
 } gun_currentState;
@@ -20,6 +24,20 @@ void gun_init()
 {
 	gun_currentState = init_st;
 	shotCount = MAX_SHOTCOUNT;
+	//TODO set our gun's frequency according to the switches. (Or do we want to do that in the game instead?)
+}
+
+bool gun_attemptShot()
+{
+	if (shotCount > NO_SHOTS_LEFT)	//If we still have shots in our clip
+	{
+		transmitter_run();			//Shoot at our specified frequency TODO set the frequency some time before this (during init() perhaps?)
+		shotCount--;				//decrement shot count
+		//TODO play shooting sound
+		return SHOT_SUCCESSFUL;		//Return that we've successfully shot
+	}
+	//TODO play empty clip sound
+	return SHOT_UNSUCCESSFUL;		//Otherwise, if our clip was empty, return that we did not successfully shoot
 }
 
 void gun_tick()
@@ -28,20 +46,19 @@ void gun_tick()
 	switch (gun_currentState)
 	{
 	case init_st:
-		gun_currentState = wait_st;
+		gun_currentState = wait_st;	//Go to our first state where we will wait for the trigger to be pulled
 		break;
 		
 	case wait_st:
-		if (gun_enabled && trigger_wantsToShoot())
+		if (gun_enabled && trigger_wantsToShoot())	//If we are enabled and the trigger has been pulled
 		{
-			transmitter_run();//TODO Change so we only shoot if we have ammo
-			trigger_clearWantsToShoot();
-			gun_currentState = shoot_st;
-			//TODO decrement shot count
+			gun_attemptShot();			//Attempt to shoot
+			trigger_clearWantsToShoot();//Clear the flag that told us the player is trying to shoot (to say we have handled it)
+			gun_currentState = shot_st;	//Go to the state where we have already shot and will decide if we need to reload
 		}
 		break;
 		
-	case shoot_st:
+	case shot_st:
 		//Check if we need to auto-reload (out of ammo), or if we should go to force-reload state
 		break;
 		
@@ -61,7 +78,7 @@ void gun_tick()
 		case wait_st:
 		break;
 		
-		case shoot_st:
+		case shot_st:
 		break;
 		
 		case force_reload_st:
